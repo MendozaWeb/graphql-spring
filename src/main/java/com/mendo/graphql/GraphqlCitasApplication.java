0483d09eb8bdaf8e6eeb793c3c3e115fc60ca82c
goodmarcus2020 <goodmarcus2020@gmail.com>
2025-11-08 17:33:16 -0500

    feat: add project base

---
+++ src/main/java/com/mendo/graphql/GraphqlCitasApplication.java
@@ -0,0 +1,13 @@
+package com.mendo.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlCitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mendo/graphql/GraphqlCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendo.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
